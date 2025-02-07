0d642c6cc629a74de2502430655ba99893a13d00
kenzychew <kenzychew@gmail.com>
2025-02-07 15:25:45 +0800

    feat: create read delete

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
 # Fruits
 
-## Concept
+## Concepts
 
-We want to learn express + mongoose work together, EJS optional
+We want to learn express + mongoose work together, (EJS optional)
 
 These examples only use 1 model -> Fruit
 
@@ -14,3 +14,22 @@ Project 3 needs 2 + 1 (User) -> embedding / referencing
 2. Connect mongoose + mongodb
    1. Add on dotenv
 3. Create models
+
+## Important stuff
+
+- learn to connect mongoose to express
+- learn to write express routes to:
+  - Create Fruit
+  - Read Fruits -> Read Single Fruit -> Not going to do search
+  - Update Fruit
+  - Delete Fruit
+
+## Create a Server route
+
+1. Know what is the URL
+2. add the code to express to show placeholder on the URL
+3. add in the mongoose part -> show response
+4. Use EJS to show the response
+   1. render the placeholder EJS
+   2. change the code to match the data
+   3. Link EJS with the data
--- server.js
+++ server.js
@@ -2,47 +2,68 @@
 require("dotenv").config(); //? https://github.com/motdotla/dotenv
 const express = require("express");
 const mongoose = require("mongoose");
-const morgan = require('morgan');
+const morgan = require("morgan");
 
 //* config
-const log = require('debug')('fruits:server')
+const log = require("debug")("fruits:server");
 const app = express();
 const port = 3000;
 mongoose.connect(process.env.MONGODB_URI);
 mongoose.set("debug", true);
-mongoose.connection.on("connected", () => { //? log connection status to terminal on start
-    log(`Connected to MongoDB ${mongoose.connection.name}.`);
-  });
+mongoose.connection.on("connected", () => {
+  //? log connection status to terminal on start
+  log(`Connected to MongoDB ${mongoose.connection.name}.`);
+});
 
-const Fruit = require('./models/Fruit');
+const Fruit = require("./models/Fruit");
 
 //* middleware
-app.use(morgan('dev'));
+app.use(morgan("dev"));
 //* following line is for EJS only -> interpret the body of the POST
 app.use(express.urlencoded({ extended: false }));
 
-
 //* routes
-app.get('/', (req, res) => {
+app.get("/", (req, res) => {
   // res.send("Hello World!");
   res.render("index.ejs");
 });
 
-app.get('/fruits/new', (req, res) => {
-    // res.send("new fruits");
-    res.render("fruits/new.ejs");
-  });
+app.get("/fruits/new", (req, res) => {
+  // res.send("new fruits");
+  res.render("fruits/new.ejs");
+});
 
-app.post('/fruits', async (req, res) => {
-    if (req.body.isReadyToEat === "on") {
-        req.body.isReadyToEat = true;
-      } else {
-        req.body.isReadyToEat = false;
-      }
-      log("body %o", req.body)
-    await Fruit.create(req.body);
-    res.redirect('fruits/new');
-})
+app.post("/fruits", async (req, res) => {
+  if (req.body.isReadyToEat === "on") {
+    req.body.isReadyToEat = true;
+  } else {
+    req.body.isReadyToEat = false;
+  }
+  log("body %o", req.body);
+  await Fruit.create(req.body);
+  res.redirect("fruits");
+});
+
+app.get("/fruits", async (req, res) => {
+  //? Mongoose code to read all the fruits?
+  const fruits = await Fruit.find({});
+
+  res.render("fruits/index.ejs", { fruits });
+});
+
+app.get("/fruits/:fruitId", async (req, res) => {
+  const { fruitId } = req.params;
+  const fruit = await Fruit.findById(fruitId);
+  res.render("fruits/show.ejs", { fruit });
+});
+
+app.delete("/fruits/:fruitId", async (req, res) => {
+  const { fruitId } = req.params;
+
+  await Fruit.findByIdAndDelete(fruitId);
+
+  res.redirect("/fruits");
+});
 
 //* listen
 app.listen(port, () => {
